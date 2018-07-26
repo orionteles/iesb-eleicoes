9f3c32909f421e529194cf70329941cb95bbeed5
Lucas Manoel <lucaslmmanoel5@gmail.com>
2018-07-26 18:06:13 -0300

    set and get

--- eleitor/Eleitor.php
+++ eleitor/Eleitor.php
@@ -17,7 +17,267 @@ class Eleitor{
     protected $numero_endereco;
     protected $id_municipio;
     protected $foto;
+     
+    
+    /**
+     * Get the value of foto
+     */ 
+    public function getFoto()
+    {
+        return $this->foto;
+    }
 
+    /**
+     * Set the value of foto
+     *
+     * @return  self
+     */ 
+    public function setFoto($foto)
+    {
+        $this->foto = $foto;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_municipio
+     */ 
+    public function getId_municipio()
+    {
+        return $this->id_municipio;
+    }
+
+    /**
+     * Set the value of id_municipio
+     *
+     * @return  self
+     */ 
+    public function setId_municipio($id_municipio)
+    {
+        $this->id_municipio = $id_municipio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numero_endereco
+     */ 
+    public function getNumero_endereco()
+    {
+        return $this->numero_endereco;
+    }
+
+    /**
+     * Set the value of numero_endereco
+     *
+     * @return  self
+     */ 
+    public function setNumero_endereco($numero_endereco)
+    {
+        $this->numero_endereco = $numero_endereco;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of bairro
+     */ 
+    public function getBairro()
+    {
+        return $this->bairro;
+    }
+
+    /**
+     * Set the value of bairro
+     *
+     * @return  self
+     */ 
+    public function setBairro($bairro)
+    {
+        $this->bairro = $bairro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of complemento
+     */ 
+    public function getComplemento()
+    {
+        return $this->complemento;
+    }
+
+    /**
+     * Set the value of complemento
+     *
+     * @return  self
+     */ 
+    public function setComplemento($complemento)
+    {
+        $this->complemento = $complemento;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of logradouro
+     */ 
+    public function getLogradouro()
+    {
+        return $this->logradouro;
+    }
+
+    /**
+     * Set the value of logradouro
+     *
+     * @return  self
+     */ 
+    public function setLogradouro($logradouro)
+    {
+        $this->logradouro = $logradouro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cep
+     */ 
+    public function getCep()
+    {
+        return $this->cep;
+    }
+
+    /**
+     * Set the value of cep
+     *
+     * @return  self
+     */ 
+    public function setCep($cep)
+    {
+        $this->cep = $cep;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of telefone
+     */ 
+    public function getTelefone()
+    {
+        return $this->telefone;
+    }
+
+    /**
+     * Set the value of telefone
+     *
+     * @return  self
+     */ 
+    public function setTelefone($telefone)
+    {
+        $this->telefone = $telefone;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of secao
+     */ 
+    public function getSecao()
+    {
+        return $this->secao;
+    }
+
+    /**
+     * Set the value of secao
+     *
+     * @return  self
+     */ 
+    public function setSecao($secao)
+    {
+        $this->secao = $secao;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of zona
+     */ 
+    public function getZona()
+    {
+        return $this->zona;
+    }
+
+    /**
+     * Set the value of zona
+     *
+     * @return  self
+     */ 
+    public function setZona($zona)
+    {
+        $this->zona = $zona;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */ 
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */ 
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */ 
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */ 
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id_eleitor
+     */ 
+    public function getId_eleitor()
+    {
+        return $this->id_eleitor;
+    }
+
+    /**
+     * Set the value of id_eleitor
+     *
+     * @return  self
+     */ 
+    public function setId_eleitor($id_eleitor)
+    {
+        $this->id_eleitor = $id_eleitor;
+
+        return $this;
+    }
 
 
     public function recuperarDados()
@@ -125,4 +385,6 @@ class Eleitor{
         $sql = "delete from eleitor where id_eleitor = $id_eleitor";
         return $conexao->executar($sql);
     }
+
+
 }
